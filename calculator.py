b243d05d7a8175e4394f44a516babdd168f3b370
cmomodo <ceesay.ml@outlook.com>
2020-12-31 09:03:53 +0000

    doing the basic steps

--- calculator.py
+++ calculator.py
@@ -9,9 +9,25 @@ e.grid(row=0, column = 0, columnspan = 3, padx = 10, pady = 10)
 
 
 def  button_click(number):
+    #e.delete(0, END)
+    current = e.get()
     e.delete(0, END)
-    e.insert(0, number)
+    e.insert(0, str(current) + str(number))
 
+def button_clear():
+    e.delete(0, END)
+
+def button_add(first_number):
+    first_number = e.get()
+    global f_num
+    f_num = int(first_number)
+    e.delete(0, END)
+    
+
+def button_equal():
+    second_number = e.get()
+    e.delete(0, END)
+    e.insert(0, f_num + int(second_number))
 #define buttons.
 button_1 = Button(root, text = "1", padx=40, pady=20, command=lambda: button_click(1))
 button_2 = Button(root, text = "2", padx=40, pady=20, command=lambda: button_click(2))
@@ -23,9 +39,9 @@ button_7 = Button(root, text = "7", padx=40, pady=20, command=lambda: button_cli
 button_8 = Button(root, text = "8", padx=40, pady=20, command=lambda: button_click(8))
 button_9 = Button(root, text = "9", padx=40, pady=20, command=lambda: button_click(9))
 button_0 = Button(root, text = "0", padx=40, pady=20, command=lambda: button_click(0))
-button_add = Button(root, text= "+", padx=39, pady=20, command=lambda: button_click())
-button_equal = Button(root, text="=", padx=79, pady=20, command=lambda: button_click())
-button_clear = Button(root, text="Clear", padx=79, pady=20, command=lambda: button_click())
+button_add = Button(root, text= "+", padx=39, pady=20, command=lambda: button_add(e.get()))
+button_equal = Button(root, text="=", padx=91, pady=20, command=lambda: button_equal)
+button_clear = Button(root, text="Clear", padx=79, pady=20, command=button_clear)
 
 
 
